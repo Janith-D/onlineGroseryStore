4ebb125c5399cb37ef44e77a0feeef9f2b722548
Janith-D <janetdgo2001@gmail.com>
2025-05-28 00:24:12 +0530

    first commit

---
+++ src/main/java/com/example/online_grocery_delivery/OnlineGroceryDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_grocery_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineGroceryDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineGroceryDeliveryApplication.class, args);
+	}
+
+}
