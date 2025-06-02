303ad92681c2bec2dceee1cbe88166e040d0a115
Janith-D <janetdgo2001@gmail.com>
2025-06-02 23:54:01 +0530

    first commit

---
+++ src/main/java/com/example/online_grocery_delivery/Services/EmailImp.java
@@ -0,0 +1,35 @@
+package com.example.online_grocery_delivery.Services;
+
+import com.example.online_grocery_delivery.Dto.EmailDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailImp implements EmailService{
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String senderEmail;
+    @Override
+    public void sendEmail(EmailDto emailDto) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(senderEmail);
+            message.setTo(emailDto.getRecipient());
+            message.setText(emailDto.getMessageBody());
+            message.setSubject(emailDto.getSubject());
+
+            javaMailSender.send(message);
+            System.out.println("Email send SUCCESSFUL!");
+        }catch (MailException e){
+            throw new RuntimeException(e);
+        }
+        }
+    }
+
---
+++ src/main/java/com/example/online_grocery_delivery/Services/EmailService.java
@@ -0,0 +1,7 @@
+package com.example.online_grocery_delivery.Services;
+
+import com.example.online_grocery_delivery.Dto.EmailDto;
+
+public interface EmailService {
+    void sendEmail(EmailDto emailDto);
+}
